e29a540805cbab0d161fd8a5cf3c2ce3d6f213b1
omid-khosroabadi <119924579+omid-khosroabadi@users.noreply.github.com>
2023-04-02 18:22:38 +0330

    Update forms.py

--- products/forms.py
+++ products/forms.py
@@ -4,10 +4,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SearchForm(forms.Form):
+    '''
+    for search products
+    '''
     search = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': _('search for mobile')}))
 
 
 class CommentForm(forms.ModelForm):
+    '''
+    The fields that the user must fill in to post a comment
+    '''
     class Meta:
         model = Comment
         fields = ['body', 'recommend', 'star']
