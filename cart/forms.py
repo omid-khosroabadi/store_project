7c7110ef4947625cc58539b8336624f5387538c1
omid-khosroabadi <119924579+omid-khosroabadi@users.noreply.github.com>
2023-04-02 18:07:59 +0330

    Update forms.py

--- cart/forms.py
+++ cart/forms.py
@@ -2,6 +2,9 @@ from django import forms
 
 
 class AddToCart(forms.Form):
+    '''
+    This function is to get the product number from the user
+    '''
     QUANTITY = ((i, str(i)) for i in range(1, 11))
     quantity = forms.TypedChoiceField(choices=QUANTITY, coerce=int)
     inplace = forms.BooleanField(required=False, widget=forms.HiddenInput)
